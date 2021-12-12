fbf1fc7b8fd2cbe3c39e5491830bd14b8f5ebb6b
Mohammad Jaha <miro.tune@gmail.com>
2021-12-12 12:24:24 +0000

    Uploading Ass

---
+++ MadLibs/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  MadLibs
+//
+//  Created by admin on 12/12/2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var adjectivField: UITextField!
+    @IBOutlet weak var verbOneField: UITextField!
+    @IBOutlet weak var verbTwoField: UITextField!
+    @IBOutlet weak var nounField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        messageLabel.text = "No Message Yet"
+    }
+    
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue){
+        let destination = unwindSegue.source as! SecondViewController
+        let adjective = destination.adjectivField.text ?? "Error"
+        let verbOne = destination.verbOneField.text ?? "Error"
+        let verbTwo = destination.verbTwoField.text ?? "Error"
+        let noun = destination.nounField.text ?? "Error"
+        
+        messageLabel.text = "We are having a perfectly \(adjective) right now. Later we will \(verbOne) and \(verbTwo) in the \(noun)"
+    }
+    
 }
 
